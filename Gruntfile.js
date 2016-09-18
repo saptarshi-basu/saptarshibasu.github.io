7379430be073469d1e2c712b029ab84d6778e90c
Saptarshi Basu <sapb2004@gmail.com>
2016-09-18 12:39:19 +0530

    New blog entries, removed AWS, spruced up GUI

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,110 +1,104 @@
 module.exports = function(grunt) {
-  // Project configuration.
-  grunt.initConfig({
-    jade: {
-      compile: {
-        options: {
-          pretty: true,
-        },
-        files: [ {
-                  src: "*.jade",
-				  dest: "./",
-                  ext: ".html",
-                  cwd: "src/jade/",
-				  expand: true
-                } ,
-				{
-                  src: "*.jade",
-                  dest: "articles/",
-                  ext: ".html",
-                  cwd: "src/jade/articles/",
-				  expand: true
-                } ]
-      }
-    },
-	less: {
-      compile: {
-        options: {
-          paths: ['test/fixtures/include']
-        },
-        files: {
-          'css/techsnippet.css': 'src/less/techsnippet.less',
-		  'css/explore.css': 'src/less/explore.less',
-		  'css/article.css': 'src/less/article.less'
-        }
-      }
-	},
-	copy: {
-		dev: {
-			files: [{
-				cwd: 'src/img/',
-				src: '*.*',
-				dest: 'img/',
-				expand: true
+	grunt.initConfig({
+		jade: {
+		  compile: {
+			options: {
+			  pretty: true,
 			},
-			{
-				cwd: 'src/font/',
-				src: '*.*',
-				dest: 'font/',
-				expand: true
-			},
-			{
-				cwd: 'src/css/',
-				src: '*min.*',
-				dest: 'css/',
-				expand: true
+			files: [ {
+					  src: "*.jade",
+					  dest: "./",
+					  ext: ".html",
+					  cwd: "src/jade/",
+					  expand: true
+					} ,
+					{
+					  src: "*.jade",
+					  dest: "articles/",
+					  ext: ".html",
+					  cwd: "src/jade/articles/",
+					  expand: true
+					} ]
+		  }
+		},
+		less: {
+			compile: {
+				options: {
+				  paths: ['src/less']
+				},
+				files: {
+				  'src/css/main.css': 'src/less/main.less'
+				}
 			}
-
-			]
-		}
-	},
-	postcss: {
-		options: {
-			map: {
-				inline: false,
-				annotation: 'css/maps/' 
+		},
+		copy: {
+			dev: {
+				files: [
+					{
+						cwd: 'src/img/',
+						src: '*.*',
+						dest: 'img/',
+						expand: true
+					},
+					{
+						cwd: 'src/fonts/',
+						src: '**/*.*',
+						dest: 'fonts/',
+						expand: true
+					},
+					{
+						cwd: 'src/jade/demo',
+						src: '*.*',
+						dest: 'demo/',
+						expand: true
+					}					
+				]
+			}
+		},
+		concat: {
+			options: {
 			},
+			dist: {
+				src: ['src/css/main.css','src/css/materialize.min.css'],
+				dest: 'src/css/techsnippet.css'
+			},
+		},
+		postcss: {
+			options: {
+				map: {
+					inline: false,
+					annotation: 'css/maps/' 
+				},
+				processors: [
+					require('autoprefixer')({browsers: ['last 3 versions']}), 
+					require('cssnano')() 
+				]
+			},
+			single_file: {
+				src: 'src/css/techsnippet.css',
+				dest: 'css/techsnippet.min.css'
+			}
+		},
+		uglify: {
+			js: {
+				files: {
+					'js/techsnippet.min.js': [
+						'src/js/jquery-3.1.0.min.js',
+						'src/js/materialize.min.js']
+				},
+				options: {
+					preserveComments: false
+				}
+			}
+		}
+    });
 
-        processors: [
-			require('autoprefixer')({browsers: ['last 3 versions']}), 
-			require('cssnano')() 
-        ]
-      },
-      dist: {
-		src: 'css/*.css',
-      }
-    },
-	babel: {
-        options: {
-            sourceMap: true,
-            presets: ['es2015']
-        },
-        dist: {
-            files: [{
-                expand: true,
-				cwd: 'src/js/',
-				ext: '.js',
-				src: ['utility.js','main.js'],
-				dest: 'js/'
-            }]
-        }
-    },
-
- browserify: {
-         dist: {
-            files: {
-               "js/techsnippet.js": ["js/main.js","js/utility.js"]
-            }
-         }
-      }});
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-postcss');
-  grunt.loadNpmTasks('grunt-babel');
-  grunt.loadNpmTasks('grunt-browserify');
-  
-  // Default task.
-  grunt.registerTask('build', 'Convert Jade templates into html templates', ['jade','less','copy','postcss','babel','browserify']);
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	  
+	grunt.registerTask('build', 'Convert Jade templates into html templates', ['jade','less','copy','concat','postcss','uglify']);
 };
